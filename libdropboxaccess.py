0243501d7352c64f7e1fcc84710dfd3b21c128de
Abhijit Kar <draj.dj@gmail.com>
2013-01-07 00:03:17 -0600

    DropboxAccessor class. OO-access

--- libdropboxaccess.py
+++ libdropboxaccess.py
@@ -2,54 +2,59 @@
 
 from dropbox import client, rest, session
 
-APP_KEY = ''
-APP_SECRET = ''
-# change to 'dropbox' if you're not using limited access
-ACCESS_TYPE = ''
-TOKEN_FILE = ''
+TOKEN_FILE = 'token.txt'
 
 # Code taken from Dropbox website tutorial
-def requestDropboxSessionToken():
-	dropbox_session = session.DropboxSession( APP_KEY, APP_SECRET, ACCESS_TYPE )
-	request_token = dropbox_session.obtain_request_token()
-	dropbox_session_url = dropbox_session.build_authorize_url( request_token )
+class DropboxAccessor:
+    """ Pass in APP_KEY, APP_SECRET, ACCESS_TYPE when instantiating """
 
-	print "url:", dropbox_session_url
-	print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
-	raw_input()
+    def __init__(self, **kwargs ):
+		self._APP_KEY = kwargs['APP_KEY']
+		self._APP_SECRET = kwargs['APP_SECRET']
+		self._ACCESS_TYPE = kwargs['ACCESS_TYPE']
+		
+    def requestDropboxSessionToken(self):
+    	dropbox_session = session.DropboxSession( self.APP_KEY, self.APP_SECRET, self.ACCESS_TYPE )
+    	request_token = dropbox_session.obtain_request_token()
+    	dropbox_session_url = dropbox_session.build_authorize_url( request_token )
+    
+    	print "url:", dropbox_session_url
+    	print "Please visit this website and press the 'Allow' button, then hit 'Enter' here."
+    	raw_input()
+    
+    	access_token = dropbox_session.obtain_access_token()
+    
+    	# TODO: Handle case where invalid data is read
+    	with open( TOKEN_FILE, 'w' ) as token_file:
+    		token_file.write( '%s\n%s' % ( access_token.key, access_token.secret ) )
+    		print "Token file '%s' written successfully" % TOKEN_FILE
+    	
+    	return ( access_token.key, access_token.secret, )
+    
+    def getDropboxSessionToken(self):
+    	""" Check if token exists in a file. If not, request a new one """
+    	token = None
+    	try:
+    		with open( TOKEN_FILE, 'r' ) as token_file:
+    			token_key = token_file.readline().strip()
+    			token_secret = token_file.readline().strip()
+    			token = ( token_key, token_secret, )
+    
+    	except IOError as e:
+    		token = self.requestDropboxSessionToken()
+    	
+    	return token
 
-	access_token = dropbox_session.obtain_access_token()
+    def getDropboxSession(self):
+    	token_key, token_secret = self.getDropboxSessionToken()
+    	dropbox_session = session.DropboxSession( self._APP_KEY, self._APP_SECRET, self._ACCESS_TYPE )
+    	dropbox_session.set_token( token_key, token_secret )
+    
+    	return dropbox_session
 
-	# TODO: Handle case where invalid data is read
-	with open( TOKEN_FILE, 'w' ) as token_file:
-		token_file.write( '%s\n%s' % ( access_token.key, access_token.secret ) )
-		print "Token file '%s' written successfully" % TOKEN_FILE
-	
-	return ( access_token.key, access_token.secret, )
-
-def getDropboxSessionToken():
-	""" Check if token exists in a file. If not, request a new one """
-	token = None
-	try:
-		with open( TOKEN_FILE, 'r' ) as token_file:
-			token_key = token_file.readline().strip()
-			token_secret = token_file.readline().strip()
-			token = ( token_key, token_secret, )
-
-	except IOError as e:
-		token = requestDropboxSessionToken()
-	
-	return token
-
-def getDropboxSession():
-	token_key, token_secret = getDropboxSessionToken()
-	dropbox_session = session.DropboxSession( APP_KEY, APP_SECRET, ACCESS_TYPE )
-	dropbox_session.set_token( token_key, token_secret )
-
-	return dropbox_session
-
-def getDropboxClient( dropbox_session ):
-	dropbox_client = client.DropboxClient( dropbox_session )
-
-	return dropbox_client
+    def getDropboxClient(self):
+        dropbox_session = self.getDropboxSession()
+    	dropbox_client = client.DropboxClient( dropbox_session )
+    
+    	return dropbox_client
 
--- pyDrBoxMusicPlayer.py
+++ pyDrBoxMusicPlayer.py
@@ -3,7 +3,15 @@
 # TODO: import selectively
 from libdropboxaccess import *
 
-dropbox_client = getDropboxClient( getDropboxSession() )
+kwargs = {
+    'APP_KEY': '',
+    'APP_SECRET':  '',
+    # change to 'dropbox' if you're not using limited access
+    'ACCESS_TYPE': '',
+}
+
+dropbox_accessor = DropboxAccessor( **kwargs )
+dropbox_client = dropbox_accessor.getDropboxClient()
 
 account_info = dropbox_client.account_info()
 for keys in account_info:
